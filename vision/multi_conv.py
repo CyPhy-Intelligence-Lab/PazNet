1990b3538aae2fe21f9dafeaeaa67dcffc2e2b71
TW <lfwutong@gmail.com>
2020-09-13 10:22:25 -0400

    no oversampling

--- vision/multi_conv.py
+++ vision/multi_conv.py
@@ -152,7 +152,9 @@ op_data, label = load_op_data()
 ts_data = load_data()
 
 # over-sampling
-balanced_ts, balanced_op = data_preprocessing.over_sampling_op(ts_data, op_data)
+#balanced_ts, balanced_op = data_preprocessing.over_sampling_op(ts_data, op_data)
+balanced_ts = ts_data
+balanced_op = op_data
 
 # one hot encoding
 onehot = pd.get_dummies(balanced_ts['goodtime'], columns=['l1', 'l2'])
