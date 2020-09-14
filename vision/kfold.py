8824cb5d84ee0d66bdb2f1ab43069f1ee52491d1
TW <lfwutong@gmail.com>
2020-09-14 11:26:51 -0400

    kfold SGD

--- vision/kfold.py
+++ vision/kfold.py
@@ -159,7 +159,7 @@ for train_index, test_index in skf.split(data_concat, label):
         model.compile(optimizer=SGD(learning_rate), loss=categorical_crossentropy,
                       metrics=[categorical_accuracy, ])
 
-        model.fit(x=[ts_scaled, op_scaled], y=onehot_train, epochs=50,
+        model.fit(x=[ts_scaled, op_scaled], y=onehot_train, epochs=100,
                   batch_size=batch_size, validation_data=([x_test_ts_scaled, x_test_op_scaled], onehot_test))
         loss, acc = model.evaluate([x_test_ts_scaled, x_test_op_scaled], onehot_test)
         accuracy.append(acc)
