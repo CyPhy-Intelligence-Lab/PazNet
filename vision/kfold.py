82c349d5ab158285c32784d9391dbd095c093ee7
TW <lfwutong@gmail.com>
2020-09-15 11:40:40 -0400

    class weights

--- vision/kfold.py
+++ vision/kfold.py
@@ -170,7 +170,7 @@ for train_index, test_index in skf.split(data_concat, label):
         model.compile(optimizer=Adam(learning_rate), loss=categorical_crossentropy,
                       metrics=[categorical_accuracy, ])
 
-        model.fit(x=[ts_scaled, op_scaled], y=onehot_train, epochs=1500,
+        model.fit(x=[ts_scaled, op_scaled], y=onehot_train, epochs=100,
                   batch_size=batch_size, validation_data=([x_test_ts_scaled, x_test_op_scaled], onehot_test),
                   class_weight={0: 2, 1: 1})
         '''
