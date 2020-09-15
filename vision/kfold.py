7e020ffe65dab65b185bf85914a2f1687acb0c5b
TW <lfwutong@gmail.com>
2020-09-15 13:23:43 -0400

    class weights

--- vision/kfold.py
+++ vision/kfold.py
@@ -172,15 +172,15 @@ for train_index, test_index in skf.split(data_concat, label):
         model.compile(optimizer=Adam(learning_rate), loss=categorical_crossentropy,
                       metrics=[categorical_accuracy, ])
 
-        model.fit(x=[ts_scaled, op_scaled], y=onehot_train, epochs=80,
+        model.fit(x=[ts_scaled, op_scaled], y=onehot_train, epochs=20,
                   batch_size=batch_size, validation_data=([x_test_ts_scaled, x_test_op_scaled], onehot_test),
                   class_weight={0: 2, 1: 1})
 
         loss, acc = model.evaluate([x_test_ts_scaled, x_test_op_scaled], onehot_test)
         loss, no_acc = model.evaluate(x=[x_test_ts_scaled.iloc[no_indices, :],
                                       x_test_op_scaled[no_indices]], y=onehot_test.iloc[no_indices, :])
-        loss, yes_acc = model.evaluate(x=[x_test_ts_scaled.iloc[no_indices, :],
-                                      x_test_op_scaled[no_indices]], y=onehot_test.iloc[no_indices, :])
+        loss, yes_acc = model.evaluate(x=[x_test_ts_scaled.iloc[yes_indices, :],
+                                      x_test_op_scaled[yes_indices]], y=onehot_test.iloc[yes_indices, :])
         '''
         model = mlp()
         model.compile(optimizer=SGD(learning_rate), loss=categorical_crossentropy,
